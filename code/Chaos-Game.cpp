4fbdc8507591339642daf4368aaa32f1fbc7986f
HussinIrfan <hussinirfan1@gmail.com>
2022-09-14 18:29:11 -0700

    Scuffed Version

--- code/Chaos-Game.cpp
+++ code/Chaos-Game.cpp
@@ -3,78 +3,95 @@
 #include <iostream>
 #include <vector> 
 
-// Make code easier to type with "using namespace"
 using namespace sf;
 using namespace std;
 
+
+Vector2f returnPoint(vector<Vector2f> v, Vector2f midpoint) //Function Returns midpoint between 2 random points
+{
+	int randNum = rand() % 3 + 0;
+	midpoint.x = (midpoint.x + v[randNum].x)/2;
+	midpoint.y = (midpoint.y + v[randNum].y)/2;
+
+	return midpoint;
+};
+
 int main()
 {
-    // Create a video mode object
-	VideoMode vm(1920, 1080);
+	srand((int)time(0));
+  
+	VideoMode vm(1920/2, 1080/2); // Create a video mode object
 
-	// Create and open a window for the game
+	RenderWindow window(vm, "Sierpinksi Triangle!!", Style::Default); // Create and open a window for the game
 
-	RenderWindow window(vm, "Sierpinksi Triangle!!", Style::Default);
+	RectangleShape rect(Vector2f{2,2}); //Set the size of rectangles and color
+	rect.setFillColor(Color::White);
 
-	RectangleShape rect(Vector2f{20,20});
+	vector<Vector2f> vertices;//First 3 clicks
 
-	vector<Vector2f> vertices;// first 3 clicks
-	vector<Vector2f> points; // 4th click
+	Vector2f point; //4th click
 
-	Vector2f clicked; //user clicks
+	Vector2f clicked; //User clicks
+	Event event;
 
-    while (window.isOpen())
+	bool endClicks = true; //Make sure user clicks a max of 4 times
+	int userClicks = 0;
+	int maxClicks = 3;
+
+    while (window.isOpen())  //Main Loop
 	{
-		/*
-		****************************************
-		Handle the players input
-		****************************************
-		*/		
 
-	//Update the scene
-
-		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		if (Keyboard::isKeyPressed(Keyboard::Escape)) //Esc closes Program
 		{
 			window.close();
 		}
-		/*
-			****************************************
-			Update the scene
-			****************************************
-		*/
-		Event event;
+
    		while (window.pollEvent(event))
    		{
-       // Request for closing the window
+ 
        		if (event.type == sf::Event::Closed)
            		window.close();
-   		}
-	
-		if (event.type == sf::Event::MouseButtonPressed)
-		{	
-    		if (event.mouseButton.button == sf::Mouse::Left)
-    		{
-        		std::cout << "the right button was pressed" << std::endl;
-        		std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-        		std::cout << "mouse y: " << event.mouseButton.y << std::endl;
 
-				clicked.x = event.mouseButton.x;
-				clicked.y = event.mouseButton.y;
-    		}
+			//Checks for a max of 4 user clicks
+			if (event.type == Event::MouseButtonPressed && endClicks == true) 
+			{
+				if (event.mouseButton.button == Mouse::Left) 
+				{
+					cout << "mouse x: " << event.mouseButton.x << endl;
+					cout << "mouse y: " << event.mouseButton.y << endl;
+					clicked.x = event.mouseButton.x;
+					clicked.y = event.mouseButton.y;
+
+					if (userClicks < maxClicks)
+					{
+						vertices.push_back(Vector2f(clicked.x, clicked.y));
+						rect.setPosition(vertices[userClicks].x, vertices[userClicks].y);
+						userClicks++;
+					}
+					else
+					{
+						point = Vector2f(clicked.x, clicked.y);
+						endClicks = false;
+					}
+				}
+				
+			}
+   		}
+
+		if (endClicks == false) //Calls function after user done inputting
+		{
+			rect.setPosition(point.x, point.y);
+			point = returnPoint(vertices, point);
 		}
 
-	rect.setPosition(clicked.x, clicked.y);
-	rect.setFillColor(Color::Red);
-
-	//Clear the scene
-	window.clear();
-	//Draw the Scene:
-	window.draw(rect);
-	window.display();
-
+		//Clear the scene
+		//window.clear();
+		
+		//Draw the Scene:
+		window.draw(rect);
+		window.display();
 
     }
 
     return 0;
-
 }
