be8363258cc0552833cebfcad39065f35a976522
HussinIrfan <hussinirfan1@gmail.com>
2022-09-16 18:31:43 -0700

    9-16 Code

--- code/Chaos-Game.cpp
+++ code/Chaos-Game.cpp
@@ -7,11 +7,14 @@ using namespace sf;
 using namespace std;
 
 
-Vector2f returnPoint(vector<Vector2f> v, Vector2f midpoint) //Function Returns midpoint between 2 random points
+vector<Vector2f> returnPoint(vector<Vector2f> v, vector<Vector2f> midpoint) //Function Returns midpoint between 2 random points
 {
-	int randNum = rand() % 3 + 0;
-	midpoint.x = (midpoint.x + v[randNum].x)/2;
-	midpoint.y = (midpoint.y + v[randNum].y)/2;
+	int randNum = rand() % 3;
+	Vector2f temp;
+
+	temp.x = (midpoint[midpoint.size() - 1].x + v[randNum].x) / 2;
+	temp.y = (midpoint[midpoint.size() - 1].y + v[randNum].y)/2;
+	midpoint.push_back(Vector2f(temp.x, temp.y));
 
 	return midpoint;
 };
@@ -28,8 +31,7 @@ int main()
 	rect.setFillColor(Color::White);
 
 	vector<Vector2f> vertices;//First 3 clicks
-
-	Vector2f point; //4th click
+	vector<Vector2f> point; //4th click
 
 	Vector2f clicked; //User clicks
 	Event event;
@@ -37,22 +39,22 @@ int main()
 	bool endClicks = true; //Make sure user clicks a max of 4 times
 	int userClicks = 0;
 	int maxClicks = 3;
-
+	//window.setVerticalSyncEnabled(true);
     while (window.isOpen())  //Main Loop
 	{
+		window.clear();
 
 		if (Keyboard::isKeyPressed(Keyboard::Escape)) //Esc closes Program
 		{
 			window.close();
 		}
 
-   		while (window.pollEvent(event))
+   		while (window.pollEvent(event)) //Checks for a max of 4 user clicks
    		{
  
-       		if (event.type == sf::Event::Closed)
-           		window.close();
+       		//if (event.type == sf::Event::Closed)  //Not sure what this does
+           		//window.close();
 
-			//Checks for a max of 4 user clicks
 			if (event.type == Event::MouseButtonPressed && endClicks == true) 
 			{
 				if (event.mouseButton.button == Mouse::Left) 
@@ -62,15 +64,15 @@ int main()
 					clicked.x = event.mouseButton.x;
 					clicked.y = event.mouseButton.y;
 
-					if (userClicks < maxClicks)
+					if (userClicks < maxClicks) //Three Clicks, stores in two seperate vectors
 					{
 						vertices.push_back(Vector2f(clicked.x, clicked.y));
-						rect.setPosition(vertices[userClicks].x, vertices[userClicks].y);
+						point.push_back(Vector2f(clicked.x, clicked.y));
 						userClicks++;
 					}
-					else
+					else //Fourth Click, bool value flips so algorithm starts
 					{
-						point = Vector2f(clicked.x, clicked.y);
+						point.push_back(Vector2f(clicked.x, clicked.y));
 						endClicks = false;
 					}
 				}
@@ -78,18 +80,18 @@ int main()
 			}
    		}
 
-		if (endClicks == false) //Calls function after user done inputting
+		if (endClicks == false) //Calls algorithm function after user done inputting
 		{
-			rect.setPosition(point.x, point.y);
 			point = returnPoint(vertices, point);
 		}
 
-		//Clear the scene
-		//window.clear();
-		
-		//Draw the Scene:
-		window.draw(rect);
-		window.display();
+		for (int i = 0; i < point.size(); i++) //Draws every point stored in point vector
+		{
+			rect.setPosition(point[i].x, point[i].y);
+			window.draw(rect);
+		}
+
+		window.display(); //Display 
 
     }
 
